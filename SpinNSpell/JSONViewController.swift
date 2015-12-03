9409769f9b39f3c76d4003cf4ea23200687914e4
Conrad Z <conradzimney@gmail.com>
2015-12-02 16:53:47 -0800

    Created JSON Files. Edited JSON screen. Aligned Info button on homepage.

---
+++ SpinNSpell/JSONViewController.swift
@@ -0,0 +1,23 @@
+//
+//  JSONViewController.swift
+//  SpinNSpell
+//
+//  Created by Conrad Zimney on 12/2/15.
+//  Copyright © 2015 Vivian. All rights reserved.
+//
+
+import UIKit
+
+class JSONViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

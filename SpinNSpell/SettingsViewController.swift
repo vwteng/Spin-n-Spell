3a785d4ae5b43885f25f98e289d2af4b49b4a187
Vivian <vwteng@uw.edu>
2015-12-03 20:29:33 -0800

    Worked on settings. Got sliders to move and update.

--- SpinNSpell/SettingsViewController.swift
+++ SpinNSpell/SettingsViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    var maxLength = 8
+    var sound = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,37 @@ class SettingsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /* Max Word Length */
+    @IBOutlet weak var maxWordLength: UILabel!
+    @IBOutlet weak var lengthSlider: UISlider!
+    
+    @IBAction func sliderChanged(sender: UISlider) {
+        var currValue = Int(sender.value)
+        maxLength = currValue
+        maxWordLength.text = "Max Word Length: \(currValue)"
+    }
+    
+    
+    /* Sounds */
+    @IBOutlet weak var soundSwitch: UISwitch!
+    
+    @IBAction func switchPressed(sender: UISwitch) {
+        if soundSwitch.on {
+            sound = true
+        } else {
+            sound = false
+        }
+    }
+    
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let vc = segue.destinationViewController as? ViewController {
+            // will need to pass settings around?
+        }
     }
     
 }

5eece03939a10d073023f86582d85b06b2ba72f7
Conrad Z <conradzimney@gmail.com>
2015-12-03 18:08:30 -0800

    Added View Controllers for About and Settings screens.

---
+++ SpinNSpell/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  SpinNSpell
+//
+//  Created by Conrad Zimney on 12/3/15.
+//  Copyright © 2015 Vivian. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

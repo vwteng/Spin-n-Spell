a9d0c49ab92fad65f767d92e74e16b101f1121e9
Morgan Evans <evansmorg@gmail.com>
2015-12-06 19:58:11 -0800

    add badges view controller

---
+++ SpinNSpell/BadgesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BadgesViewController.swift
+//  SpinNSpell
+//
+//  Created by iGuest on 12/6/15.
+//  Copyright © 2015 Vivian. All rights reserved.
+//
+
+import UIKit
+
+class BadgesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}

a10e0981504c6e0befa09fcf00ba97a3ef26ee14
Conrad Z <conradzimney@gmail.com>
2015-12-12 15:16:41 -0800

    Change Topic view controller activity indicator is working

--- SpinNSpell/ChangeTopicViewController.swift
+++ SpinNSpell/ChangeTopicViewController.swift
@@ -22,9 +22,55 @@ class ChangeTopicViewController: UIViewController, UITableViewDataSource, UITabl
     
     var topics = [NSDictionary]()
     
+    private var images: [UIImage] = [UIImage]()
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.registerClass(TopicCell.self, forCellReuseIdentifier : cellTableIdentifier)
+        
+        tableView.delegate = self
+        activityIndicator.startAnimating()
+        
+        for topic in topics {
+            
+            let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+            
+            /* Create session, and optionally set a NSURLSessionDelegate. */
+            let session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+            
+            let URL = NSURL(string: topic["startingImage"] as! String)
+            let request = NSMutableURLRequest(URL: URL!)
+            request.HTTPMethod = "GET"
+            
+            /* Start a new Task */
+            let task = session.dataTaskWithRequest(request, completionHandler: { (data : NSData?, response : NSURLResponse?, error : NSError?) -> Void in
+                if (error == nil) { // Success
+                    let statusCode = (response as! NSHTTPURLResponse).statusCode
+                    print("URL Session Task Succeeded: HTTP \(statusCode)")
+                    
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        let image = UIImage(data: data!)
+                        self.images.append(image!)
+                        if self.images.count == self.topics.count {
+                            print("now I can call reload data")
+                            // load picker in here
+                            self.activityIndicator.hidesWhenStopped = true
+                            self.activityIndicator.stopAnimating()
+                            self.tableView.reloadData()
+                        }
+                        
+                    })
+                }
+                else { // Failure
+                    print("URL Session Task Failed: %@", error!.localizedDescription);
+                }
+            })
+            task.resume()
+            //words.append(word.uppercaseString)
+        }
+        
         navigationController!.setNavigationBarHidden(false, animated:true)
         let infoButton:UIButton = UIButton(type: UIButtonType.Custom) as UIButton
         infoButton.addTarget(self, action: "GoToInfoSegue", forControlEvents: UIControlEvents.TouchUpInside)
@@ -66,11 +112,12 @@ class ChangeTopicViewController: UIViewController, UITableViewDataSource, UITabl
         let cell = tableView.dequeueReusableCellWithIdentifier(cellTableIdentifier, forIndexPath: indexPath) as! TopicCell
         let imageView = UIImageView(frame: CGRectMake(50, 10, 5, 5))
         let rowData = topics[indexPath.row]
-        // Code for loading an image from the web
-        let url = NSURL(string: (rowData["startingImage"] as? String)!)
-        let data = NSData(contentsOfURL: url!)
-        imageView.image = UIImage(data: data!)
-        // End loading image code
+        
+        // This needs to change to just load the image from the stored array
+        if !activityIndicator.isAnimating() {
+            imageView.image = images[indexPath.row]
+        }
+        
         cell.imageView?.image = imageView.image
         cell.topic = (rowData["topic"]! as? String)!
         return cell
